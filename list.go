7966f4e29f7aacc587d64a4983d95099c1764d3a
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-03-30 23:39:45 +0900

    Implement doEach

--- list.go
+++ list.go
@@ -4,56 +4,10 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"os"
-	"path"
-	"path/filepath"
-	"sort"
 )
 
-func makeMatchingFunc(files []string) func(string) bool {
-	if files == nil || len(files) <= 0 {
-		return func(string) bool { return true }
-	}
-	_files := make([]string, len(files))
-	for i, f := range files {
-		_files[i] = filepath.ToSlash(f)
-	}
-	sort.Strings(_files)
-
-	return func(name string) bool {
-		index := sort.Search(len(_files), func(i int) bool {
-			if m, err := path.Match(_files[i], name); err == nil && m {
-				return true
-			}
-			return _files[i] >= name
-		})
-		if index < 0 || index >= len(_files) {
-			return false
-		}
-		m, err := path.Match(_files[index], name)
-		return err == nil && m
-	}
-}
-
 func List(fileName string, files []string, verbose bool, w io.Writer) error {
-	zipReader, err := NewZipReadWrapper(fileName)
-	if err != nil {
-		return err
-	}
-	defer zipReader.Close()
-
-	isMatch := makeMatchingFunc(files)
-
-	sc := zipReader.NewScanner()
-	for sc.Scan() {
-		name, err := sc.Name()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			continue
-		}
-		if !isMatch(name) {
-			continue
-		}
+	return doEach(fileName, files, func(name string, sc *ZipScanner) error {
 		if verbose {
 			mode := sc.Mode()
 			bit := fs.FileMode(01000)
@@ -70,6 +24,6 @@ func List(fileName string, files []string, verbose bool, w io.Writer) error {
 				sc.Modified.Format("01 _2 15:04"))
 		}
 		fmt.Fprintln(w, name)
-	}
-	return nil
+		return nil
+	})
 }
--- wrapper.go
+++ wrapper.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"archive/zip"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
 
 	"github.com/zetamatta/go-windows-mbcs"
 )
@@ -73,4 +78,55 @@ func (e *ZipScanner) Name() (string, error) {
 	} else {
 		return f.Name, nil
 	}
+}
+
+func makeMatchingFunc(files []string) func(string) bool {
+	if files == nil || len(files) <= 0 {
+		return func(string) bool { return true }
+	}
+	_files := make([]string, len(files))
+	for i, f := range files {
+		_files[i] = filepath.ToSlash(f)
+	}
+	sort.Strings(_files)
+
+	return func(name string) bool {
+		index := sort.Search(len(_files), func(i int) bool {
+			if m, err := path.Match(_files[i], name); err == nil && m {
+				return true
+			}
+			return _files[i] >= name
+		})
+		if index < 0 || index >= len(_files) {
+			return false
+		}
+		m, err := path.Match(_files[index], name)
+		return err == nil && m
+	}
+}
+
+func doEach(fileName string, files []string, f func(name string, sc *ZipScanner) error) error {
+	zipReader, err := NewZipReadWrapper(fileName)
+	if err != nil {
+		return err
+	}
+	defer zipReader.Close()
+
+	isMatch := makeMatchingFunc(files)
+
+	sc := zipReader.NewScanner()
+	for sc.Scan() {
+		name, err := sc.Name()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			continue
+		}
+		if !isMatch(name) {
+			continue
+		}
+		if err := f(name, sc); err != nil {
+			return err
+		}
+	}
+	return nil
 }
