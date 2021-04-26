cd515f2cb3bbe24e39d458f5be0c3f1215fa8a38
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-04-26 22:09:17 +0900

    Fix: -t: directory-mark was not shown

--- list.go
+++ list.go
@@ -9,15 +9,20 @@ import (
 func list(fileName string, files []string, verbose bool, w io.Writer) error {
 	return doEach(fileName, files, func(name string, sc *ZipScanner) error {
 		if verbose {
+			if sc.FileInfo().IsDir() {
+				w.Write([]byte{'d'})
+			} else {
+				w.Write([]byte{'-'})
+			}
 			mode := sc.Mode()
 			bit := fs.FileMode(01000)
-			for _, c := range []byte("drwxrwxrwx") {
+			for _, c := range []byte("rwxrwxrwx") {
+				bit >>= 1
 				if (mode & bit) > 0 {
 					w.Write([]byte{c})
 				} else {
 					w.Write([]byte{'-'})
 				}
-				bit >>= 1
 			}
 			fmt.Fprintf(w, "%8d %s ",
 				sc.CompressedSize64,
